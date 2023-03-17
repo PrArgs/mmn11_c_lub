aa88d5a74dc7578492d3d47fd40220a525515c19
Roi Argaman <zebroi.arg@gmail.com>
2023-03-17 14:38:05 +0200

    transfar_draft

---
+++ count_bits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define count_bits;
+
+/* 
+count_bits: 
+return nuber of on bits in an even locacation in an unsigned long 
+ */
+
+int count_bits(unsigned long value)
+{
+    int count = 0;
+
+    for(i=long)
+    {       
+    return count;
+    }
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include my_string.c
+#include ount_bits.c
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
