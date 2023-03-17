dd201100931bf9a0b09d2f77448360631d246477
Roi Argaman <zebroi.arg@gmail.com>
2023-03-17 11:26:51 +0200

    setup

---
+++ my_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define my_strcmp;
+#define ny_strcpy;
+#define my_strchr;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+int my_strcmp()
+{
+    return 0;
+}
+
+char *my_strcpy()
+{
+    return NULL;
+}
+
+char *my_strchr(){
+    return NULL;
+}
